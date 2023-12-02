096137d93713b9a402a454eaf8fa48182d6147fa
Dawittaff <dawittaff41@gmail.com>
2023-12-02 13:59:16 +0100

    SignUp-Screen

--- DP/backend/Routes/userRoutes.js
+++ DP/backend/Routes/userRoutes.js
@@ -28,4 +28,24 @@ userRouter.post(
     res.send(401).send({ message: 'Invalid mail or password' });
   })
 );
+
+userRouter.post(
+  '/signup',
+  expressAsyncHandler(async (req, res) => {
+    // create a new user from mongoose model
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    });
+    const user = await newUser.save();
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user),
+    });
+  })
+);
 export default userRouter;
