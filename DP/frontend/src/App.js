7eba55b8be9f8afa28489f3c4f61bfacd5ad2d14
Dawittaff <dawittaff41@gmail.com>
2023-12-05 15:26:40 +0100

    Create-Order Screen

--- DP/frontend/src/App.js
+++ DP/frontend/src/App.js
@@ -17,6 +17,7 @@ import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignupScreen from './screens/SignupScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -86,6 +87,7 @@ function App() {
               <Route path="/signup" element={<SignupScreen />}></Route>
               <Route path="/signup" element={<SignupScreen />}></Route>
               <Route path="/placeorder" element={<PlaceOrderScreen />}></Route>
+              <Route path="/order/:id" element={<OrderScreen />}></Route>
               <Route
                 path="/shipping"
                 element={<ShippingAddressScreen />}
